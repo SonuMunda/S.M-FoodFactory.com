0010f35f8d20b68c3d4d50dcd1802e51ac4b247e
Sonu Munda <79930169+SonuMunda@users.noreply.github.com>
2021-03-03 21:10:20 +0530

    Delete js directory

--- js/menufunction.js
+++
@@ -1,8 +0,0 @@
-const openmenu=()=>{
-    document.getElementById('menu').style.display="block";
-    document.getElementById('close-menu').style.display="block";
-}
-const closemenu=()=>{
-    document.getElementById('menu').style.display="none";
-    document.getElementById('close-menu').style.display="none";
-}
