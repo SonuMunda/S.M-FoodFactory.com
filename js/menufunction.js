5a8c7fc51974acda8ecf7b6173348f2589fa4bdb
Sonu Munda <79930169+SonuMunda@users.noreply.github.com>
2021-03-03 18:49:48 +0530

    Add files via upload

---
+++ js/menufunction.js
@@ -0,0 +1,8 @@
+const openmenu=()=>{
+    document.getElementById('menu').style.display="block";
+    document.getElementById('close-menu').style.display="block";
+}
+const closemenu=()=>{
+    document.getElementById('menu').style.display="none";
+    document.getElementById('close-menu').style.display="none";
+}
